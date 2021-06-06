0a08e139bc012c712ed040dc5ffe3f79ba89989d
juju1337 <julian@lamberty.org>
2021-06-06 19:57:21 +0200

    rename power_limit and requested_power

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ class HendiHeater(ActorBase):
     onoff_pin = Property.Select("On/Off GPIO", options=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27])
     power_pin = Property.Select("Power Control GPIO", options=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27])
     pwm_freq = Property.Number("PWM Frequency (Hz)", configurable=True, default_value=100, unit="Hz")
-    max_power = Property.Number("Limit Max Power (%)", configurable=True, default_value=100, unit="%")
+    power_limit = Property.Number("Limit Maximum Power (%)", configurable=True, default_value=100, unit="%")
     power = 0
     pwm = None
     pwm_running = False
@@ -19,8 +19,8 @@ class HendiHeater(ActorBase):
         GPIO.output(int(self.onoff_pin), 0)
         self.pwm = GPIO.PWM(int(self.power_pin, int(self.pwm_freq))
 
-    def on(self, power = 0):
-        self.power = min(int(power), int(self.max_power))
+    def on(self, requested_power = 0):
+        self.power = min(int(requested_power), int(self.power_limit))
         if self.pwm_running == False:
             self.pwm.start(int(self.power))
             self.pwm_running = True
@@ -29,8 +29,8 @@ class HendiHeater(ActorBase):
         if(self.power > 0):
             GPIO.output(int(self.onoff_pin), 1)
 
-    def set_power(self, power = 0):
-        self.power = min(int(power), int(self.max_power))
+    def set_power(self, requested_power = 0):
+        self.power = min(int(requested_power), int(self.power_limit))
         if self.pwm_running == False:
             self.pwm.start(int(self.power))
             self.pwm_running = True
