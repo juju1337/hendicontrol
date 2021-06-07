74d9be4583bbe61e4b99265785dfc690b3c4f972
Julian Lamberty <julian@lamberty.org>
2021-06-07 13:47:03 +0200

    add controller section

--- __init__.py
+++ __init__.py
@@ -17,10 +17,10 @@ except Exception as e:
 
 @cbpi.actor
 class HendiHeater(ActorBase):
-    onoff_pin = Property.Select("On/Off GPIO", options=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27])
-    power_pin = Property.Select("Power Control GPIO", options=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27])
-    pwm_freq = Property.Number("PWM Frequency (Hz)", configurable=True, default_value=100, unit="Hz")
-    power_limit = Property.Number("Limit Maximum Power (%)", configurable=True, default_value=100, unit="%")
+    onoff_pin = Property.Select("On/Off GPIO", options = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27])
+    power_pin = Property.Select("Power Control GPIO", options = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27])
+    pwm_freq = Property.Number("PWM Frequency (Hz)", configurable = True, default_value = 100, unit = "Hz")
+    power_limit = Property.Number("Maximum Power (%)", configurable = True, default_value = 100, unit = "%")
     power = 0
     pwm = None
     pwm_running = False
@@ -54,3 +54,20 @@ class HendiHeater(ActorBase):
         self.pwm.stop()
         self.pwm_running = False
         GPIO.output(int(self.onoff_pin), 0)
+
+@cbpi.controller
+class GradientPowerControl(KettleController):
+    gradient_factor = Property.Number("Gradient Factor", configurable = True, default_value = 1, unit = "")
+    lookback_time = Property.Number("Lookback Time (s)", configurable = True, default_value = 15, unit = "s")
+    mash_power_limit = Property.Number("Maximum Mash Power (%)", configurable = True, default_value = 50, unit = "%")
+    boil_power = Property.Number("Boil Power (%)", configurable = True, default_value = 40, unit = "%")
+    boil_threshold = Property.Number("Boil Power Threshold", configurable = True, default_value = 90, unit = "°C")
+
+    def init(self):
+        pass
+
+    def run(self):
+
+    def stop(self):
+        super(KettleController, self).stop()
+        self.heater_off()
